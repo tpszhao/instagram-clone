6409ec3aedd92b22c3ccf4bc84d58b1e7a52511f
tpszhao <tonyzhao111@gmail.com>
2020-02-23 19:20:56 -0800

    replaced HomePage css with styled Compoennts

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import styles from './HomePage.module.css'
+import styled from 'styled-components'
 import {LatestPhoto} from 'Components'
+
+const Container = styled.div`
+    margin: auto;
+    display: flex;
+    position: relative;
+    justify-content: center;
+    @media only screen and (max-width: 585px){
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+`;
+
+const SideBar = styled.div`
+    margin: 10px 16px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const SideItem = styled.div`
+    width: 296px;
+    height: 224px;
+    border: 1px solid rgb(219,219,219);
+    border-radius: 3px;
+    position: sticky;
+    top: 78px;
+    @media only screen and (max-width: 585px){
+        min-width: 100%;
+    }
+`;
 
 export default function HomePage() {
     return (
-        <div className={styles.container}>
+        <Container>
             <LatestPhoto/>
-            <div className={styles.sideBar}>
-                <div className={styles.sideItem}>placeholder</div>
-            </div>
-        </div>
+            <SideBar>
+                <SideItem>placeholder</SideItem>
+            </SideBar>
+        </Container>
     )
 }
