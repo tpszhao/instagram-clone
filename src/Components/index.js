70281fa629dda39bc4e0d2e2b2d939011b816346
tpszhao <tonyzhao111@gmail.com>
2020-02-24 21:30:48 -0800

    initial commit

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,13 @@
 export {default as NavBar} from './NavBar' 
+
 export {default as LatestPhoto} from './LatestPhoto' 
 export {default as Card} from './Card' 
 export {default as CardLoader} from './CardLoader' 
+
 export {default as PhotoGrid} from './PhotoGrid' 
 export {default as GridItem} from './GridItem' 
 export {default as GridLoader} from './GridLoader' 
+
+export {default as Favourites} from './Favourites' 
+
 
