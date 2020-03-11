0bacde7de4966a65a2811dacde96ea8003a4f50f
tpszhao <tonyzhao111@gmail.com>
2020-03-11 04:00:35 -0700

    changed modal default opacity

--- src/Components/CustomModal.jsx
+++ src/Components/CustomModal.jsx
@@ -4,7 +4,7 @@ import Modal from "react-modal";
 const modalStyle = {
     overlay: {
         zIndex: 50,
-        backgroundColor: "rgb(219, 219, 219,0.5)"
+        backgroundColor: "rgb(219, 219, 219,0.7)"
     },
     content: {
         top: "50%",
@@ -15,7 +15,7 @@ const modalStyle = {
         padding: "none",
         marginRight: "-50%",
         transform: "translate(-50%, -50%)",
-        backgroundColor: "rgb(219, 219, 219,0.9)"
+        backgroundColor: "rgb(219, 219, 219,0.7)"
     }
 };
 Modal.setAppElement("#root");
