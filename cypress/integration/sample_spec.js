3690e4f0d0bb45094eb8fd2cf3e23b7a0f4f8de1
tpszhao <tonyzhao111@gmail.com>
2020-03-20 18:40:32 -0700

    changed tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -5,9 +5,7 @@ describe("example test", ()=>{
     it("go to explore page", ()=>{
         cy.visit("localhost:3000");
         cy.get('[data-cy=explore_page_icon]').click();
-        cy.location().should(loc =>{
-            expect(loc.pathname).to.include("explore")
-        })
+        cy.url().should('include', "explore")
     })
 
     it(`search for ${searchValue}`,()=>{
@@ -15,12 +13,10 @@ describe("example test", ()=>{
             .type(searchValue)
             .should('have.value',searchValue)
             .type('{enter}')
-        cy.location().should(loc =>{
-            expect(loc.pathname).to.include(searchValue)
-        })
+        cy.url().should('include', searchValue)
     })
 
-    
+
 
 
 })
