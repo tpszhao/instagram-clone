4944e49893821345b939d9ca506aa1c5549d3437
tpszhao <tonyzhao111@gmail.com>
2020-04-05 10:04:16 -0700

    changed stype for statList in GridHeader

--- src/Components/Grid/GridHeader.tsx
+++ src/Components/Grid/GridHeader.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Container, Image, Title, Stats } from './GridHeader.styles'
+import React from "react";
+import { Container, Image, Title, Stats } from "./GridHeader.styles";
 
-interface Props { 
-    src: string;
-    title: string;
-    statList: any[];
+interface Props {
+  src: string;
+  title: string;
+  statList: string[];
 }
 
-export default function GridHeader({src,title,statList=[]}:Props) {    
-    return (
+export default function GridHeader({ src, title, statList = [] }: Props) {
+  return (
     <Container>
-        <Image src={src} alt=""/>
-        <Title>{title}</Title>
-        <Stats>
-        {statList.map((stat,i)=><span key={i}>{stat}</span>)}
-        </Stats>
+      <Image src={src} alt="" />
+      <Title>{title}</Title>
+      <Stats>
+        {statList.map((stat, i) => (
+          <span key={i}>{stat}</span>
+        ))}
+      </Stats>
     </Container>
-    )
+  );
 }
-
