ab9a7895ebc00bc20833402597570e0a7d461f58
tpszhao <tonyzhao111@gmail.com>
2020-04-14 17:55:00 -0700

    changed infiniteLoaderReducer to TypeScript

--- src/reducers/infiniteLoaderReducer.ts
+++ src/reducers/infiniteLoaderReducer.ts
@@ -7,7 +7,41 @@ const START_LOADING = "START_LOADING";
 const NEXT_PAGE = "NEXT_PAGE";
 const REQUEST_ERROR = "REQUEST_ERROR";
 
-export const initialState = {
+interface BasicRouteType {
+  dataList: any[];
+  page: number;
+  hasMore: boolean;
+}
+
+interface ExploreType extends BasicRouteType {
+  heightList: string[];
+}
+
+interface GridPageType extends BasicRouteType {
+  route: string | null;
+  keyword: string | null;
+  total: number | null;
+}
+
+interface SearchPageType {
+  route: string;
+  keyword: string | null;
+  photos: BasicRouteType & { total: number | null };
+  collections: BasicRouteType & { total: number | null };
+  [key: string]: any;
+}
+
+interface StateType {
+  allowFetching: boolean;
+  isLoading: boolean;
+  "/": BasicRouteType;
+  "/explore": ExploreType;
+  gridPage: GridPageType;
+  searchPage: SearchPageType;
+  [key: string]: any;
+}
+
+export const initialState: StateType = {
   allowFetching: false,
   isLoading: false,
   "/": {
@@ -49,7 +83,23 @@ export const initialState = {
   },
 };
 
-const PRE_FETCHING_CLEANUP = (state, payload) => {
+interface PayloadType {
+  route: string;
+  dataList: any[];
+  total: number | null;
+  keyword: string | null;
+  searchType: string;
+}
+
+interface ActionType {
+  type: string;
+  payload: PayloadType;
+}
+
+const PRE_FETCHING_CLEANUP = (
+  state: StateType = initialState,
+  { payload }: ActionType
+) => {
   const { route } = payload;
   let result = { ...state, allowFetching: true };
   let key;
@@ -70,36 +120,44 @@ const PRE_FETCHING_CLEANUP = (state, payload) => {
   return { ...result, [key]: initialState[key] };
 };
 
-const NEXT_PAGE_ACTION = (state, payload) => {
+const NEXT_PAGE_ACTION = (
+  state: StateType = initialState,
+  { payload }: ActionType
+) => {
   const { route, dataList, total, keyword, searchType } = payload;
-  let prevRouteState;
-  let newRouteState;
   const stopLoadingState = { ...state, isLoading: false };
   switch (route) {
-    case "/":
-    case "/explore":
-      prevRouteState = state[route];
-      newRouteState = {
+    case "/": {
+      const prevRouteState = state[route];
+      const newRouteState = {
         dataList: [...prevRouteState.dataList, ...dataList],
         page: prevRouteState.page + 1,
         hasMore: !!dataList.length,
       };
-      if (route === "/explore") {
-        const newHeightList = dataList.map(
-          () => `${generateRandomNumber(200, 400)}px`
-        );
-        newRouteState.heightList = [
-          ...prevRouteState.heightList,
-          ...newHeightList,
-        ];
-      }
       return {
         ...stopLoadingState,
         [route]: newRouteState,
       };
+    }
+    case "/explore": {
+      const prevRouteState = state[route];
+      const newHeightList = dataList.map(
+        () => `${generateRandomNumber(200, 400)}px`
+      );
+      const newRouteState = {
+        dataList: [...prevRouteState.dataList, ...dataList],
+        page: prevRouteState.page + 1,
+        hasMore: !!dataList.length,
+        heightList: [...prevRouteState.heightList, ...newHeightList],
+      };
 
-    case "/search/:searchType/:searchValue":
-      prevRouteState = state.searchPage;
+      return {
+        ...stopLoadingState,
+        [route]: newRouteState,
+      };
+    }
+    case "/search/:searchType/:searchValue": {
+      const prevRouteState = state.searchPage;
       const prevSearchTypeState = prevRouteState[searchType];
       const newSearchTypeState = {
         dataList: [...prevSearchTypeState.dataList, ...dataList],
@@ -108,7 +166,7 @@ const NEXT_PAGE_ACTION = (state, payload) => {
         total,
         route,
       };
-      newRouteState = {
+      const newRouteState = {
         ...prevRouteState,
         [searchType]: newSearchTypeState,
         keyword,
@@ -117,9 +175,10 @@ const NEXT_PAGE_ACTION = (state, payload) => {
         ...stopLoadingState,
         searchPage: newRouteState,
       };
-    default:
-      prevRouteState = state.gridPage;
-      newRouteState = {
+    }
+    default: {
+      const prevRouteState = state.gridPage;
+      const newRouteState = {
         route,
         keyword,
         total,
@@ -131,21 +190,25 @@ const NEXT_PAGE_ACTION = (state, payload) => {
         ...stopLoadingState,
         gridPage: newRouteState,
       };
+    }
   }
 };
 
-const infiniteLoaderReducer = (state = initialState, action) => {
+const infiniteLoaderReducer = (
+  state: StateType,
+  action: ActionType
+): StateType => {
   let result;
   switch (action.type) {
     case ALLOW_FETCHING:
-      result = PRE_FETCHING_CLEANUP(state, action.payload);
+      result = PRE_FETCHING_CLEANUP(state, action);
       return result;
     case PAUSE_FETCHING:
       return { ...state, allowFetching: false };
     case START_LOADING:
       return { ...state, isLoading: true };
     case NEXT_PAGE:
-      result = NEXT_PAGE_ACTION(state, action.payload);
+      result = NEXT_PAGE_ACTION(state, action);
       return result;
     case REQUEST_ERROR:
       return { ...state, allowFetching: false, isLoading: false };
